693de5caaafd42309558dee8a29d475884a75bec
nafeesahyounis <nafeesah.youniss@gmail.com>
2019-10-11 09:08:53 +0000

    Testing making cards flip over

---
+++ new2.js
@@ -0,0 +1,83 @@
+
+
+//game variables
+
+let cardElements = document.getElementsByClassName('game-card');
+let cardElementsArray = [...cardElements];
+let imgElements = document.getElementsByClassName('game-card-img');
+let imgElementsArray = [...imgElements];
+let openedCards = [];
+
+
+//start game
+
+function startGame(){
+    for(let i = 0; i < cardElementsArray.length; i++){
+        cardElementsArray[i].addEventListener('click',displayCard)
+}}
+
+function displayCard(){
+    this.children[0].classList.toggle('show-img');
+    cardOpen(this);
+
+}
+
+function cardOpen(card){
+    openedCards.push(card);
+    let len = openedCards.length;
+    if(len >2) {
+        openedCards[i].addEventListener('click',unflip);
+        function unflip(){
+
+            openedCards[i].classList.remove('show-img');
+
+        }
+
+        }
+}
+
+
+/*function unflip(){
+    for(i=0; i<len; i++) {
+            openedCards[i].children[0].classList.remove("show-img")
+    /*openedCards[0].classList.remove("show-img", "open");
+    openedCards[1].classList.remove("show-img", "open");
+    openedCards[0].classList.add("game-card-img");
+    openedCards[1].classList.add("game-card-img");
+    }
+
+}
+function matched() {
+    openedCards[0].classList.add("match");
+    openedCards[1].classList.add("match");
+    openedCards[0].classList.remove("show", "open");
+    openedCards[1].classList.remove("show", "open");
+    matchedCards.push(openedCards[0]);
+    matchedCards.push(openedCards[1]);
+    openedCards = [];
+    if(matchedCards.length == 16) {
+        endGame();
+    }
+}
+
+function unmatched() {
+    openedCards[0].classList.add("unmatched");
+    openedCards[1].classList.add("unmatched");
+    disable();
+    setTimeout(function() {
+        openedCards[0].classList.remove("show", "open", "unmatched");
+        openedCards[1].classList.remove("show", "open", "unmatched");
+        openedCards[0].children[0].classList.remove('show-img');
+        openedCards[1].children[0].classList.remove('show-img');
+        enable();
+        openedCards = [];
+
+    }, 1100) */
+
+
+
+window.onload = function () {
+    setTimeout(function() {
+        startGame()
+    }, 1200);
+}
