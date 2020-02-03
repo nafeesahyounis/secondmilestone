fec6d739702762f71856c4fb3308f472f3aaef24
nafeesahyounis <nafeesah.youniss@gmail.com>
2020-02-03 16:54:09 +0100

    trying to append images to document via javascript in order to resolve shuffle bug

--- debug.js
+++ debug.js
@@ -1,51 +1,46 @@
+var allImages = [
+    "island.png",
+    "cat-reading.png",
+    "cuty-cats.png",
+    "sleeping-kitty.png",
+    "little-cat.png",
+    "cat.png",
+    "pirate.png",
+    "ship.png",
 
-    function Monster(image,NORMAL,SCARED,state) {
-      this.image = image;
-      this.NORMAL = NORMAL;
-      this.SCARED = SCARED;
-      this.state = state;
-      this.render = function (){
-          //go to current state
-          switch(this.state)
-          {
-              case this.NORMAL:
-              drawingSurface.drawImage(
+];
 
-              monsterImage,
-               0, 0, 64, 64,
-              0, 0, 64, 64);
-          break;
-          case this.SCARED:
-        drawingSurface.drawImage
-        (
-          monsterImage,
-          64, 0, 64, 64,
-          0, 0, 64, 64);
 
-          }
-        }
+function createImages(){
+    //create images
+
+    // counter
+    var i = 0;
+
+    // create object
+    imageObj = new Image(100,200);
+
+
+    // start preloading
+    for(i=0; i<=3; i++)
+    {
+        imageObj.src=allImages[i];
     }
 
-var monster1 = new Monster ("monsterStates.png", 0, 1, 0);
- //Set up the canvas and drawing surface
-    var canvas = document.querySelector("canvas");
-    var drawingSurface = canvas.getContext("2d");
-    //Load the monster's image
-    var monsterImage = new Image();
-    monsterImage.addEventListener("load", monster1.render, false);
-    monsterImage.src = monster1.image;
-    //Change the monster's state by pressing and releasing a key
-    window.addEventListener("click", keydownHandler, false);
-    window.addEventListener("keyup", keyupHandler,false);
+    console.log(allImages[i]);
 
-    function keydownHandler(event){
-        //when a key is pressed, change monster's state to SCARED
+    document.body.appendChild(allImages[i]);
 
-        monster1.state = monster1.SCARED;
-        monster1.render();
-    }
+}
 
-    function keyupHandler(event){
-        monster1.state = monster1.NORMAL;
-        monster1.render();
-    }
+function createOneImage(){
+    var myImage = new Image(100, 200);
+    myImage.src = 'cat.png';
+    document.body.appendChild(myImage);
+}
+
+window.onload = function () {
+    setTimeout(function() {
+        createImages();
+    }, 1200);
+}
